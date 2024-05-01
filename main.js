1692bf70dfa696c1690c667c3d26459efac5abc7
Azamatjon <azamatergasev01@gmail.com>
2024-05-01 11:47:20 +0500

    create page homework

---
+++ main.js
@@ -0,0 +1,32 @@
+
+// qo'shimcha task 
+// let arr = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]; 
+// let word = "ABCCED";
+// let new_arr = [];
+
+// arr.forEach(item => {
+//     new_arr.push(...item);
+// });
+
+// let result = [];
+// for (let i = 0; i < word.length; i++) {
+//     if(new_arr.includes (word[i])){ {
+//         result.push(word[i])
+//     }
+// }
+// }
+// if (result.join("") === word) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// qoshimcha task folsy va truthy ni alohida objectga olish  kerak.
+
+
+// let arr = [false, 1, 10, "", null, "sultonqul", 1.13, 0];
+// let falsy = arr.filter(item => Boolean(item) === false) 
+// let truthy = arr.filter(item => Boolean(item) === true) 
+// let obj = {falsy : falsy, truthy : truthy}
+
+// console.log(obj);
